61c9bed967ff81c9e309787802d20a208e9bc9be
sgrieve <s.grieve@ed.ac.uk>
2016-11-04 11:32:19 +0000

    getting correct utm zones

--- dl.py
+++ dl.py
@@ -19,13 +19,17 @@ def get_OTids():
         return list(set(IDs))
 
 
-def get_short_name(ID):
+def metadata_URL(ID):
     base_URL = ('http://opentopo.sdsc.edu/datasetMetadata?otCollectionID={0}&' +
                 'format=text')
 
     ID = ID[:2] + ID[5:]
 
-    URL = base_URL.format(ID)
+    return base_URL.format(ID)
+
+
+def get_short_name(ID):
+    URL = metadata_URL(ID)
 
     metadata = urllib2.urlopen(URL).read(2000)
     metadata = metadata.split('\n')
@@ -33,6 +37,47 @@ def get_short_name(ID):
     for line in metadata:
         if 'Short Name:' in line:
             return line.split(': ')[1].strip()
+
+
+def check_south(proj4):
+    '''
+    Check if a proj4 string has the south flag. Returns true if it is a
+    utm south zone, false if not.
+    '''
+    if 'south' in proj4.lower():
+        return True
+    else:
+        return False
+
+
+def get_UTM_zone(EPSG):
+    '''
+    Use spatialreference.org to convert a EPSG code into a UTM zone.
+    returns a tuple with the zone number and a boolean indicating if
+    it is southern hemisphere (True) or northern (False)
+    '''
+    URL = 'http://spatialreference.org/ref/epsg/{0}/proj4/'.format(EPSG)
+
+    proj4 = urllib2.urlopen(URL).read()
+    south = check_south(proj4)
+
+    return (proj4.split('+zone=')[1].split()[0].strip(), south)
+
+
+def get_EPSG_code(ID):
+    '''
+    Assumes horizontal EPSG code is always reported first
+    '''
+    URL = metadata_URL(ID)
+
+    metadata = urllib2.urlopen(URL).read(2000)
+    metadata = metadata.split('\n')
+
+    for line in metadata:
+        if 'EPSG' in line:
+            EPSG_str = line.split('[EPSG:')[1].strip()
+            # this parses out the EPSG code
+            return ''.join([s for s in EPSG_str if s.isdigit()])
 
 
 def build_URLs(short_name):
@@ -51,8 +96,7 @@ def test_URLs(URLs):
     '''
     Test if the converted urls are valid.
     returns a tuple of bools with true == valid url and false == invalid.
-    Index 0 is the pointcloud (always valid) and Index 1
-    is the raster (does not always exist)
+    Index 0 is the pointcloud and Index 1 is the raster.
     '''
     result = []
     for URL in URLs:
@@ -69,4 +113,5 @@ def download(URL):
     pass
 
 
-print test_URLs(build_URLs(get_short_name('OTLAS.082013.26910.1')))
+print get_UTM_zone(get_EPSG_code('OTLAS.082013.26910.1'))
+#print test_URLs(build_URLs(get_short_name('OTLAS.082013.26910.1')))
